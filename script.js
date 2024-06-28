b5816df53b7b4fbd1c97a34beced45393825f38e
gcoolL <145269702+gcoolL@users.noreply.github.com>
2024-06-28 03:02:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,120 @@
+let currentsong = ""
+
+let lyrics = {["Couldn't beat her smile, it stirred up all the media
+Secret side, I wanna know it, so mysterious
+Even that elusive side, part of her controlled area
+Complete and perfect
+All you say is a bunch of lies
+Dear miss genius idol, unmatched
+(You're my savior, you're my saving grace)
+
+
+What did you eat today?
+What book do you love?
+Whenever you go out for fun, tell me, where do you go?
+Haven't eaten anything
+It's a secret, unknown
+Any questions you're facing, always acting so vaguely
+
+
+So unconcerned, although you brightly glow
+Any seemingly unveiled secrets are as sweet as honey
+Confusing, why, why, why?
+Essential lie, lie, lie
+So, what is your type of guy? Any partner?
+So, now, answer this
+
+
+"I don't have any idea how I could love anyone" (Hey! Hey!)
+"I don't seem to know what it signifies" (Hey! Hey! Hey! Hey!)
+Cannot find out if it's true or it's a lie (Hey! Hey! Hey! Hey!)
+Once again, there's somebody who's fallen for the words and cues
+Made him lose his head over you
+See pop shows near Brussels
+Get tickets as low as $212
+You might also like
+YOASOBI - アイドル (Idol) (Romanized)
+Genius Romanizations
+When Emma Falls in Love (Taylor’s Version) [From The Vault]
+Taylor Swift
+So Long, London
+Taylor Swift
+
+That emotion melts all hearts, all eyes on you
+'Cause you are perfect, the most ultimate idol
+Unrivalled, will not appear again
+It's the brightest star reborn, yes, indeed
+Ah, using that smiling face
+That "I love you" again
+Now, everybody is lured and captivated by you
+The pupil that you got
+The words you vocalize
+Even when untrue, it's your perfected Ai
+
+Right, right, we all know she's very special, yes
+We had lost the fight before it started, so impressed
+Miss, I'm such a star
+We're serving as support to her grace
+Cannot tell me everything was because of her
+No, it's not right
+Out of line
+How can we not feel jealous while being around
+It's not a joke, you know, right?
+So, I cannot forgive you for that
+Completely deny
+Imperfect you that I sight
+Myself, no pardon allowed
+I won't allow anyone if it's not you, strongest of all
+
+That emotion seized all hearts worshipping you
+Yes, indeed
+So strong, it's you, unrivalled idol
+There cannot be weaknesses to find
+The brightest star is residing in you
+The gaps and shortcomings, don't show 'em
+Dammit, dammit (Hey! Hey!)
+Parts nobody wants to know should remain hidden (Hey! Hey! Hey! Hey!)
+One and only
+If it's different, no way, no way (Hey! Hey!)
+Such a true love, it's the realest Ai (Hey!)
+
+
+Showing this smile, my own weapon boiling media
+Keeping everything about my secret deep inside
+"I'm in love with you," my career is built on such a lie
+It's the way I know to show my love, without a doubt
+Running down, my sweat is flowing
+Cleanest aqua, right?
+Ruby hidden under my eyelids where it resides
+I sing and dance around
+Look at me, I'm Maria
+So, lying surely is the greatest kind of love
+
+
+I recall no one that loved me whole before
+And I've not been in love with anybody before
+Now, the lies I'm making up
+I'm hoping that a day comes when they all become true
+And I keep wishing they do
+[Chorus: ikura & REAL AKIBA BOYZ]
+One day, I will hold everything that I pursue
+Yes, I am so greedy, true voracious idol
+So, sincerely, what I'm wishing for is to love each of you with all my heart
+And so, today, I lie again (Hey! Hey! Hey! Hey!)
+The words I vocalize inside of me (Hey! Hey! Hey! Hey!)
+I'm wishing that one day they come true (Hey! Hey! Hey! Hey!)
+Up to this day, I've not been able to let you (Hey! Hey! Hey! Hey!)
+And you hear me saying those meaningful words
+
+
+I, I said it at last
+I know it's not a lie as I'm voicing these words
+I love you
+(Hey! Hey! Hey! Hey!
+Hey! Hey! Hey! Hey!
+Hey! Hey! Hey! Hey!
+Hey! Hey! Hey! Hey!
+You're my savior, my true savior, my saving grace)"]}
+
+
 
